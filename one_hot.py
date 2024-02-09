2e48910a6527d7f7de70d5e7542dd270e5ba4a03
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2024-02-09 11:48:02 +0300

    Update and rename one_hot to one_hot.py

--- one_hot.py
+++ one_hot.py
@@ -1,16 +1,12 @@
 #Задача 44: В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца. Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без get_dummies?
 
-##One_Hot с get_dummies
-
 import pandas as pd
 import random
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
-pd.get_dummies(data['whoAmI'])
 
-##One_Hot без get_dummies
 
 from sklearn.preprocessing import OneHotEncoder
 ohe = OneHotEncoder()
@@ -18,3 +14,19 @@ transformed = ohe.fit_transform(data[['whoAmI']])
 data[ohe.categories_[0]] = transformed.toarray()  
 data.drop('whoAmI', axis= 1 , inplace= True )
 print(data)
+
+##Еще один вариант
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+st = set(lst)
+strn = ""
+print(f'       {st}')
+for i in range(len(lst)):
+    for k in st:
+        if k == lst[i]:
+            strn += "1      "
+        else: strn += "0      "
+    print(f'{i} {lst[i]}    {strn}')
+    strn = ""
