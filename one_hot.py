f90293c69edbaf73e94d0049c2ec54b540995467
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2024-02-09 16:18:53 +0300

    Update one_hot.py

--- one_hot.py
+++ one_hot.py
@@ -1,29 +1,29 @@
 #Задача 44: В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца. Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без get_dummies?
 
+import random
 import pandas as pd
-import random
-lst = ['robot'] * 10
-lst += ['human'] * 10
-random.shuffle(lst)
-data = pd.DataFrame({'whoAmI':lst})
+def oneHot():
+  lst = ['robot'] * 10
+  lst += ['human'] * 10
+  random.shuffle(lst)
 
+  st = set(lst)
+  st = list(st)
+  col = []
+  tbl = []
+  data = {}
 
-from sklearn.preprocessing import OneHotEncoder
-ohe = OneHotEncoder()
-transformed = ohe.fit_transform(data[['whoAmI']])
-data[ohe.categories_[0]] = transformed.toarray()  
-data.drop('whoAmI', axis= 1 , inplace= True )
-print(data)
+  for k in st:
+      for i in lst:
+          if k == i:
+            col.append(1)
+          else: col.append(0)
 
-##Еще один вариант
-import random
-st = set(lst)
-strn = ""
-print(f'       {st}')
-for i in range(len(lst)):
-    for k in st:
-        if k == lst[i]:
-            strn += "1      "
-        else: strn += "0      "
-    print(f'{i} {lst[i]}    {strn}')
-    strn = ""
+      tbl.append(col)
+      col = [] 
+  for i in range(len(st)):
+    data[st[i]] = tbl[i]
+  return data
+
+df = pd.DataFrame(oneHot())
+df
